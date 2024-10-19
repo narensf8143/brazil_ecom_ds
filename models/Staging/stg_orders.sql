28cc1cdfeea28e8d0d04fe7b243a577bb52fd9b7
Narendra Kumar <narensf8143@gmail.com>
2024-10-19 13:01:14 +0000

    stages done

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -6,7 +6,7 @@ TO_date(ORDER_PURCHASE_TIMESTAMP) Ord_Purchase_dtm,
 TO_date(ORDER_DELIVERED_CARRIER_DATE) Ord_Dely_Ship_dtm,
 TO_date(ORDER_DELIVERED_CUSTOMER_DATE) Ord_dely_dtm,
 TO_date(ORDER_ESTIMATED_DELIVERY_DATE) ord_est_dely_dtm,
-,current_date() as Dw_Updt_Dte
+current_date() as Dw_Updt_Dte
 
 
 from {{ source('braz_ecom', 'orders') }}
